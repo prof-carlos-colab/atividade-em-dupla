82b3a43d6c0c0f94c2c03d5b83f7a169a230dd5d
prof-carlos <128708765+prof-carlos@users.noreply.github.com>
2024-10-07 12:06:06 +0000

    Resolvendo conflito.

--- arquivo_2.py
+++ arquivo_2.py
@@ -1,4 +1,20 @@
 print("Anderson")
 
-print("Continuando o projeto...")
+print("Ele Continua o projeto...")
 
+print("Fim do projeto.")
+
+# Resolução de conflito.
+# git rebase
+
+# Apague as linhas indesejadas.
+
+# git add .
+
+# git commit -m "Mensagem"
+
+# git rebase --continue
+
+# git pull 
+
+# git push
